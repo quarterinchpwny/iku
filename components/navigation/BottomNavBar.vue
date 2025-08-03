8da78f1fb4b14c77bdd210a59727a44a461ceae3
John Galindez <jgalindez@chanzglobal.com>
2025-08-03 15:15:27 +0800

    added nav logo

--- components/navigation/BottomNavBar.vue
+++ components/navigation/BottomNavBar.vue
@@ -1,24 +1,61 @@
 <template>
-  <div class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-50 md:hidden">
-    <nav class="flex justify-around h-16 items-center">
-      <NuxtLink to="/" class="flex flex-col items-center text-xs text-gray-700 hover:text-blue-600 active:text-blue-600">
-        <span>Map</span>
-      </NuxtLink>
-      <NuxtLink to="/projects" class="flex flex-col items-center text-xs text-gray-700 hover:text-blue-600 active:text-blue-600">
-        <span>Projects</span>
-      </NuxtLink>
-      <NuxtLink to="/about" class="flex flex-col items-center text-xs text-gray-700 hover:text-blue-600 active:text-blue-600">
-        <span>About</span>
+  <div class="fixed bottom-0 left-0 right-0 z-50 pb-4 shadow-lg md:hidden">
+    <nav class="flex h-16 items-center justify-around">
+      <NuxtLink
+        v-for="(icon, index) in navIcons"
+        :key="index"
+        :to="`/${icon.name.toLowerCase()}`"
+        class="flex flex-col items-center text-gray-600 hover:text-blue-600"
+        active-class="router-link-active"
+      >
+        <div class="flex flex-col items-center text-orange-600">
+          <Icon :name="icon.icon" size="32" v-if="!icon.isMainIcon" />
+          <span v-else class="text-3xl font-bold"> {{ icon.txt || icon.name }} </span>
+          <div class="pt-1 text-xs">{{ icon.name }}</div>
+        </div>
       </NuxtLink>
     </nav>
   </div>
 </template>
 
 <script setup>
+const navIcons = [
+  {
+    name: 'Home',
+    icon: 'carbon:home',
+    isMainIcon: false
+  },
+  {
+    name: 'Community',
+    icon: 'carbon:search',
+    isMainIcon: false
+  },
+  {
+    name: `let's go!`,
+    icon: 'carbon:search',
+    txt: 'ᜆᜍ',
+    isMainIcon: true
+  },
+  {
+    name: 'Stats',
+    icon: 'carbon:chart-rose',
+    isMainIcon: false
+  },
+  {
+    name: 'Profile',
+    icon: 'carbon:user',
+    isMainIcon: false
+  }
+];
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .router-link-active {
   color: #2563eb; /* blue-600 */
+}
+nav {
+  padding-bottom: constant(safe-area-inset-bottom); /* iOS 11.0 */
+  padding-bottom: env(safe-area-inset-bottom); /* Modern iOS */
+  background-color: black;
 }
 </style>
