49ee73c09384f554a4ada8cc623a4707e9f04b3b
John Francis Agustin Galindez <jgalindez@chanzglobal.com>
2025-08-08 10:50:30 +0800

    added basic implementation of dot matrix

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>this is index</div>
+  <div class="flex h-screen w-full flex-col p-2">
+    <ExtraDotMatrix
+      numberText="5°C"
+      :activeDotPositions="[
+        { row: 10, col: 2 },
+        { row: 0, col: 1 }
+      ]"
+    />
+  </div>
 </template>
 
 <script setup></script>
