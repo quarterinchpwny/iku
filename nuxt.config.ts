8111fde92c05588fca43ad4d458cf0da85c3639e
John Galindez <jgalindez@chanzglobal.com>
2025-08-09 18:48:54 +0800

    saved progress

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,9 +20,7 @@ export default defineNuxtConfig({
     'shadcn-nuxt'
   ],
   icon: {
-    serverBundle: {
-      collections: ['carbon'] // <!--- this
-    }
+    serverBundle: 'local'
   },
   shadcn: {
     /**
